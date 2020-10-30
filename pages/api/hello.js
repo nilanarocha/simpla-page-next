da0523472e45f2cd6eaa76522cd2675d4830fb4a
Nilana Rocha <nilanaarquitetura@gmail.com>
2020-10-30 16:34:08 +1100

    adding api routes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({
+    title: 'Lorem ispum',
+    image:
+      'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
+    article: 'ahhahahhahhahahha',
+  });
+};
